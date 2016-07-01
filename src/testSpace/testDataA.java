dfd3f5f15cdd31ef143da153192828df8c9e19e6
ShreyaRavi <shreyaravi7@gmail.com>
2016-07-01 15:58:32 -0400

    Multidimensional Array Test Added

--- src/testSpace/testDataA.java
+++ src/testSpace/testDataA.java
@@ -29,6 +29,14 @@ public class testDataA implements HDF5Serializable{
 	public String stringTest;
 	public boolean booleanTest;
 	public testDataB subclassTest = new testDataB();
+	public int[][][][] multiArrTest3 = {{{{1,2},{3,4},{5,6}},
+		{{7,8},{9,10},{11,12}},
+		{{13,14},{15,16},{17,18}},
+		{{19,20},{21,22},{23,24}}},
+		{{{1,2},{3,4},{5,6}},
+		{{7,8},{9,10},{11,12}},
+		{{13,14},{15,16},{17,18}},
+		{{19,20},{21,22},{23,24}}}};
 
 	public testDataA(int val) {
 		integerTest = val;
