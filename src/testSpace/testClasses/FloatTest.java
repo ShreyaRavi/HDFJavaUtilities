16750f8fd5b3a26f61b7b9205a96504d5a68b771
ShreyaRavi <shreyaravi7@gmail.com>
2016-07-06 17:15:32 -0400

    Added JUnit Automated Tests for primitives

---
+++ src/testSpace/testClasses/FloatTest.java
@@ -0,0 +1,21 @@
+package testSpace.testClasses;
+
+import HDFJavaUtils.interfaces.HDF5Serializable;
+
+public class FloatTest implements HDF5Serializable {
+	
+	public float test;
+	
+	public FloatTest() {
+		
+	}
+	
+	public FloatTest(float val) {
+		test = val;
+	}
+	
+	public float getData() {
+		return test;
+	}
+
+}
--- src/testSpace/testDataA.java
+++ src/testSpace/testDataA.java
@@ -27,7 +27,7 @@ public class testDataA implements HDF5Serializable{
 	public Map<Integer, Double> mapTest = new HashMap<Integer, Double>();
 	public String stringTest;
 	public boolean booleanTest;
-	public int[][][][] multiArrTest3 = {{{{1,2},{3,4},{5,6}},
+	public float[][][][] multiArrTest3 = {{{{1,2},{3,4},{5,6}},
 		{{7,8},{9,10},{11,12}},
 		{{13,14},{15,16},{17,18}},
 		{{19,20},{21,22},{23,24}}},
